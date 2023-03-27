5e52d00c3222b89bc91c2b9e808ab521c39bb97f
scarysss <108612656+scarysss@users.noreply.github.com>
2023-03-27 11:20:54 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -25,9 +25,13 @@ MSection:NewToggle("Loop Teleportation", "Teleport you to player everytime", fun
     end
 end)
 MSection:NewTextBox("Teleport to", "Teleport you to chosen player", function(T)
-    while teleportloop == true do
+    if teleportloop == true then
+        while teleportloop == true do
+            player.Character.HumanoidRootPart.CFrame = game:GetService("Players")[T].Character.HumanoidRootPart.CFrame
+        end
+    else
         player.Character.HumanoidRootPart.CFrame = game:GetService("Players")[T].Character.HumanoidRootPart.CFrame
-    end
+    end)
 end)
 
 
